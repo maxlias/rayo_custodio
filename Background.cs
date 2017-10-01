cee0830cea2d39e34fa885aaa79dd98f5742f299
Maximiliano Giuliani <maxgyu@gmail.com>
2017-10-01 16:35:22 -0300

    Added background and destroyArea script

---
+++ Background.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+    // Scroll speed
+    public float speed = 0.1f;
+
+    void Update()
+    {
+        // Value of Y change from 0 to 1 by time. return to 0 if it becomes 1 and repeat.
+        float y = Mathf.Repeat(Time.time * speed, 1);
+
+        // Create offset that shift value of Y
+        Vector2 offset = new Vector2(0, y);
+
+        // Set up offset to materials
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
