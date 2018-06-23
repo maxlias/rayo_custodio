6514d0739b35cc13b5f5aa6547d3ca687c941c59
Maximiliano Giuliani <maxgyu@gmail.com>
2018-06-23 18:20:30 -0300

    Added Emitters Scripts

---
+++ WaveEmitter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveEmitter : MonoBehaviour {
+
+    public GameObject[] waves;
+    private int currentWave;
+
+    void Start()
+    {
+        StartCoroutine("EmitWave");
+    }
+
+    IEnumerator EmitWave()
+    {
+        if (waves.Length == 0)
+        {
+            yield break;
+        }
+
+        while (true)
+        {
+
+            
+            GameObject wave = (GameObject)Instantiate(waves[currentWave], transform.position, Quaternion.identity);
+
+    
+            wave.transform.parent = transform;
+
+           
+            while (wave.transform.childCount != 0)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+
+         
+            Destroy(wave);
+          
+            if (waves.Length <= ++currentWave)
+            {
+                currentWave = 0;
+            }
+
+        }
+    }
+}
